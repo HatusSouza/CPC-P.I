33ffc25463f368b65eb103b6f19b40b669168900
HatusSouza <86630383+HatusSouza@users.noreply.github.com>
2021-11-05 17:09:45 -0300

    Add files via upload
    adicionado o quem somos e o jquerry pop up

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.nossa-historia').magnificPopup({ delegate: 'a',type:'image'});
+  });
+
+  $(document).ready(function() {
+    $('.link-video').magnificPopup({type:'iframe'});
+  })
